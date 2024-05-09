ef6a3702d235f45fe50b3aed3c61ef5c7546fb8d
SYR1337 <76217993+SYR2004@users.noreply.github.com>
2024-05-09 19:12:48 +0800

    Update CvarSystem.lua

--- CvarSystem.lua
+++ CvarSystem.lua
@@ -1,99 +1,211 @@
 -- SYR1337
-assert(ffi, "filesystem error: ffi is not open, please open ffi")
-ffi.cdef([[
-    void* GetProcAddress(uintptr_t, const char*);
-    typedef void*(*fnCreateInterface)(const char*, void*);
-]])
+assert(ffi, "cvar system error: ffi is not open, please open ffi")
+if not pcall(ffi.sizeof, "struct CUtlLinkedList") then
+    ffi.cdef([[
+        void* GetProcAddress(uintptr_t, const char*);
+        typedef void*(*fnCreateInterface)(const char*, void*);
+        typedef struct Color {
+            uint8_t r, g, b, a;
+        } Color;
+
+        typedef struct Vector2D {
+            float x, y;
+        } Vector2D;
+
+        typedef struct Vector {
+            float x, y, z;
+        } Vector;
+
+        typedef struct Vector4D {
+            float x, y, z, w;
+        } Vector4D;
+
+        typedef struct CConCommand {
+            const char* szName;
+            const char* szHelpString;
+            uint64_t nFlags;
+            uint64_t nUnk1;
+            uint64_t nUnk2;
+            uint64_t nUnk3;
+            uint64_t nUnk4;
+        } CConCommand;
+
+        typedef struct CConVar {
+            const char* szName;
+            struct CConVar* pNext;
+            char pad_01[0x10];
+            const char* szDescription;
+            uint32_t nType;
+            uint32_t nRegistered;
+            uint32_t nFlags;
+            uint32_t m_unk3;
+            uint32_t m_nCallbacks;
+            uint32_t m_unk4;
+            union {
+                bool Bool;
+                short Int16;
+                uint16_t Uint16;
+                int Int;
+                uint32_t Uint32;
+                int64_t Int64;
+                uint64_t Uint64;
+                float Float;
+                double Double;
+                const char* String;
+                struct Color Color;
+                struct Vector2D Vector2D;
+                struct Vector Vector3D;
+                struct Vector4D Vector4D;
+                struct Vector Angles;
+            } Value;
+
+            union {
+                bool Bool;
+                short Int16;
+                uint16_t Uint16;
+                int Int;
+                uint32_t Uint32;
+                int64_t Int64;
+                uint64_t Uint64;
+                float Float;
+                double Double;
+                const char* String;
+                struct Color Color;
+                struct Vector2D Vector2D;
+                struct Vector Vector3D;
+                struct Vector4D Vector4D;
+                struct Vector Angles;
+            } OldValue;
+        } CConVar;
+
+        typedef struct CUtlLinkedListElement {
+            struct CConVar* element;
+            uint16_t iPrevious;
+            uint16_t iNext;
+        } CUtlLinkedListElement;
+
+        typedef struct CUtlMemory {
+            struct CUtlLinkedListElement* pMemory;
+            int nAllocationCount;
+            int nGrowSize;
+        } CUtlMemory;
+        
+        typedef struct CUtlLinkedList {
+            struct CUtlMemory memory;
+            uint16_t iHead;
+            uint16_t iTail;
+            uint16_t iFirstFree;
+            uint16_t nElementCount;
+            uint16_t nAllocated;
+            struct CUtlLinkedListElement* pElements;
+        } CUtlLinkedList;
+
+        typedef struct IEngineCvar {
+            char pad_01[0x40];
+            struct CUtlLinkedList listCvars;
+        } IEngineCvar;
+    ]])
+end
 
 return setmetatable({
-    IFileSystem = ffi.cast("fnCreateInterface",
-        ffi.C.GetProcAddress(mem.GetModuleBase("filesystem_stdio.dll"), "CreateInterface")
-    )("VFileSystem017", nil)
-}, {
-    __index = {
-        Get = function(this)
-            return this.IFileSystem
-        end,
+    IEngineCvar = setmetatable({
+        IEngineCvar = ffi.cast("struct IEngineCvar*", ffi.cast("fnCreateInterface",
+            ffi.C.GetProcAddress(mem.GetModuleBase("tier0.dll"), "CreateInterface")
+        )("VEngineCvar007", nil))
+    }, {
+        __index = {
+            Get = function(self)
+                return self.IEngineCvar
+            end,
 
-        IsValid = function(this)
-            return this.IFileSystem ~= ffi.NULL
-        end,
+            IsValid = function(self)
+                return self.IEngineCvar ~= ffi.NULL
+            end,
 
-        CallVFunc = function(this, nIndex, szType, ...)
-            if not this:IsValid() then
-                return nil
-            end
+            GetCvars = function(self)
+                if not self:IsValid() then
+                    return {}
+                end
 
-            local pVtable = ffi.cast("void***", this:Get())[0]
-            return ffi.cast(szType, pVtable[nIndex])(this:Get(), ...)
-        end,
-
-        Open = function(this, szFileName, szOptions, szPathId)
-            if not this:IsValid() then
-                return nil
-            end
-
-            local hFile = this:CallVFunc(78, "void*(__thiscall*)(void*, const char*, const char*, int, const char*)", szFileName, szOptions or "r", 0, szPathId or "game")
-            if not hFile or hFile == ffi.NULL then
-                print(("filesystem error: failed open file: %s"):format(szFileName))
-                return nil
-            end
-
-            return setmetatable({
-                hFile = hFile,
-                bAvailable = true
-            }, {
-                __index = {
-                    Get = function(self)
-                        if not self.bAvailable then
-                            print(("filesystem error: failed get file: %s, file already close"):format(szFileName))
-                            return nil
-                        end
-
-                        return self.hFile
-                    end,
-
-                    GetSize = function(self)
-                        if not self.bAvailable then
-                            print(("filesystem error: failed get file size: %s, file already close"):format(szFileName))
-                            return nil
-                        end
-
-                        return this:CallVFunc(18, "uint32_t(__thiscall*)(void*, void*)", self:Get())
-                    end,
-
-                    Read = function(self, nSize)
-                        if not self.bAvailable then
-                            print(("filesystem error: failed read file: %s, file already close"):format(szFileName))
-                            return nil
-                        end
-
-                        if nSize and type(nSize) ~= "number" then
-                            print(("filesystem error: failed read file: %s, wrong file size"):format(szFileName))
-                            return nil
-                        end
-
-                        local nReadSize = nSize or self:GetSize()
-                        local pBuffer = ffi.new(("uint8_t[%s]"):format(nReadSize))
-                        local nReadedSize = this:CallVFunc(79, "int32_t(__thiscall*)(void*, void*, uint32_t, uint32_t, void*)", pBuffer, nReadSize, nReadSize, self:Get())
-                        if nReadedSize < nReadSize then
-                            print(("filesystem error: failed read file: %s, readed: %s, need: %s"):format(szFileName, nReadedSize, nReadSize))
-                            return nil
-                        end
-
-                        return pBuffer
-                    end,
-
-                    Close = function(self)
-                        if not self.bAvailable then
-                            return
-                        end
-
-                        this:CallVFunc(14, "void(__thiscall*)(void*, void*)", self:Get())
-                        self.bAvailable = false
+                local arrCvars = {}
+                local listCvar = self.IEngineCvar.listCvars
+                for nIndex = 0, listCvar.memory.nAllocationCount - 1 do
+                    local pConVar = listCvar.memory.pMemory[nIndex].element
+                    if not pConVar then
+                        goto continue
                     end
-                }
-            })
+
+                    table.insert(arrCvars, pConVar)
+                    ::continue::
+                end
+
+                return arrCvars
+            end,
+
+            FindCvar = function(self, szName)
+                if not self:IsValid() then
+                    return nil
+                end
+
+                for _, pConVar in pairs(self:GetCvars()) do
+                    if szName == ffi.string(pConVar.szName) then
+                        return pConVar
+                    end
+                end
+
+                return nil
+            end
+        }
+    })
+}, {
+    __index = function(self, szCvarName)
+        local pConVar = self.IEngineCvar:FindCvar(szCvarName)
+        if not pConVar then
+            return nil
         end
-    }
+
+        self[szCvarName] = setmetatable({
+            pRawConvar = pConVar
+        }, {
+            __index = {
+                int = function(this, nValue)
+                    if nValue then
+                        this.pRawConvar.Value.Int = nValue
+                        return
+                    end
+
+                    return this.pRawConvar.Value.Int
+                end,
+
+                bool = function(this, bValue)
+                    if bValue then
+                        this.pRawConvar.Value.Bool = bValue
+                        return
+                    end
+
+                    return this.pRawConvar.Value.Bool
+                end,
+
+                float = function(this, flValue)
+                    if flValue then
+                        this.pRawConvar.Value.Int = flValue
+                        return
+                    end
+
+                    return this.pRawConvar.Value.Float
+                end,
+
+                string = function(this, szValue)
+                    if szValue then
+                        this.pRawConvar.Value.String = szValue
+                        return
+                    end
+
+                    return ffi.string(this.pRawConvar.Value.String)
+                end,
+            }
+        })
+
+        return self[szCvarName]
+    end
 })
